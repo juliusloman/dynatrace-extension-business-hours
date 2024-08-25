b91e1440f564d66fcb1b4866b18b4ee3b58ed0ec
Julius Loman <julius.loman@alanata.sk>
2024-08-25 17:10:20 +0200

    Landing dashboard

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="business_hours",
-      version="0.0.12",
+      version="0.0.15",
       description="Business hours EF2 extension",
       author="Dynatrace",
       packages=find_packages(),
