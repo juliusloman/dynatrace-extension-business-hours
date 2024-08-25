80c70434b9e607ba3d93d687a2b834bb02fcc2c7
Julius Loman <julius.loman@alanata.sk>
2024-08-25 17:09:05 +0200

    Negative values support

--- business_hours/__main__.py
+++ business_hours/__main__.py
@@ -1,7 +1,7 @@
 from dynatrace_extension import Extension, Status, StatusValue
 import croniter
 from datetime import datetime
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from dacite import from_dict
 from typing import List
 import icalendar
@@ -14,6 +14,7 @@ class CronBH:
     level: str
     cron: str
     weight: float = 1
+    generate_negative: bool = False
     weight_outside: float = None
 @dataclass
 class CalendarBH:
@@ -23,8 +24,10 @@ class CalendarBH:
     use_summary: bool
     weight: float = 1
     weight_outside: float = None
+    generate_negative: bool = False
     rical: icalendar = None
     last_refresh: int = 0
+    event_summaries: set = field(default_factory=set)
 
 @dataclass
 class BHConfig:
@@ -38,6 +41,7 @@ class ExtensionImpl(Extension):
     def initialize(self):
         self.metric_key = "business_hours"
         self.metric_level_dimension = "level"        
+        self.negative_prefix = "-"
         self.bh_config = from_dict(data_class=BHConfig, data=self.activation_config.config)        
         self.refresh_calendars()
 
@@ -52,30 +56,51 @@ class ExtensionImpl(Extension):
 
     def process_crons(self):
         for cron in self.bh_config.crons:
-            self.logger.info(f"Generating {cron.level} of {cron.cron}")
+            self.logger.info(f"Generating business hour metric by {cron.level} of {cron.cron}")
             
             if croniter.croniter.match(cron.cron, datetime.now()):
                 self.report_metric(self.metric_key, cron.weight, dimensions={self.metric_level_dimension: cron.level})
-            elif cron.weight_outside:
-                self.report_metric(self.metric_key, cron.weight_outside, dimensions={self.metric_level_dimension: cron.level})
+            else:
+                if cron.weight_outside:
+                    self.report_metric(self.metric_key, cron.weight_outside, dimensions={self.metric_level_dimension: cron.level})
+                if cron.generate_negative:
+                    self.report_metric(self.metric_key, cron.weight, dimensions={self.metric_level_dimension: self.negative_prefix + cron.level})                            
 
     def process_calendars(self):
         for calendar in self.bh_config.calendars:
-            self.logger.info(f"Processing {calendar.calendar_url}")
+            self.logger.info(f"Generating business hour metric from calendar {calendar.calendar_url}")
             try:
                 current_events = calendar.rical.at(date=datetime.now())
                 if current_events:
                     if calendar.use_summary:
+                        active_event_summaries = set()
                         for event in current_events:
                             event_summary = event.get("SUMMARY")
+                            active_event_summaries.add(event_summary)
                             if event_summary:
                                 self.report_metric(self.metric_key, calendar.weight, dimensions={self.metric_level_dimension: event_summary})
                             else:
                                 self.report_metric(self.metric_key, calendar.weight, dimensions={self.metric_level_dimension: calendar.level})
+
+                        if calendar.generate_negative:                            
+                            self.logger.info(f"Generating negative values for inactive summaries {calendar.event_summaries - active_event_summaries}")
+                            for level in calendar.event_summaries - active_event_summaries:
+                                self.report_metric(self.metric_key, calendar.weight, dimensions={self.metric_level_dimension: self.negative_prefix + level})                            
                     else:
                         self.report_metric(self.metric_key, calendar.weight, dimensions={self.metric_level_dimension: calendar.level})
-                elif calendar.weight_outside:
-                    self.report_metric(self.metric_key, calendar.weight_outside, dimensions={self.metric_level_dimension: calendar.level})
+                else:
+                    if calendar.weight_outside:
+                        self.report_metric(self.metric_key, calendar.weight_outside, dimensions={self.metric_level_dimension: calendar.level})
+
+                    if calendar.generate_negative:
+                        if calendar.use_summary:
+                            self.logger.info(f"Generating negative values for inactive summaries {calendar.event_summaries}")
+                            for level in calendar.event_summaries:                                
+                                self.report_metric(self.metric_key, calendar.weight, dimensions={self.metric_level_dimension: self.negative_prefix + level})                            
+                        else:
+                            self.logger.info(f"Generating negative value for level {calendar.level}")
+                            self.report_metric(self.metric_key, calendar.weight, dimensions={self.metric_level_dimension: self.negative_prefix + calendar.level})
+
             except Exception as e:
                 self.logger.error(f"Error {e} when processing calendar {calendar.calendar_url}")
 
@@ -85,9 +110,13 @@ class ExtensionImpl(Extension):
                 try:
                     self.logger.info(f"Loading ical from {calendar.calendar_url}")
                     ical_response = requests.get(calendar.calendar_url)
-                    ical_response.raise_for_status()                    
-                    calendar.rical = recurring_ical_events.of(icalendar.Calendar.from_ical(ical_response.text))
+                    ical_response.raise_for_status()
+                    ical = icalendar.Calendar.from_ical(ical_response.text)
+                    calendar.rical = recurring_ical_events.of(ical)
                     calendar.last_refresh = time.time()
+                    for event in ical.walk('VEVENT'):
+                        calendar.event_summaries.add(str(event.get("SUMMARY")))
+                    self.logger.info(f"Found summaries {calendar.event_summaries}")
                 except Exception as e:
                     self.logger.error(f"Error {e} for {calendar.calendar_url}")
             else:
